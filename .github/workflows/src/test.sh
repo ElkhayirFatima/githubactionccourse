36d67931b9f84ba368f0020115ae10602f4c8915
ElkhayirFatima <fatima.elkhayir.ensao@ump.ac.ma>
2025-12-29 12:47:18 +0100

    Fixe les errors

--- .github/workflows/src/test.sh
+++ .github/workflows/src/test.sh
@@ -1,20 +1,13 @@
- #!/bin/bash
+#!/bin/bash
 
- #src/test.sh
- EXPECTED = "Hello, Test!"
+# src/test.sh
+EXPECTED="Hello, Test!"
+OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
 
- OUTPUT =$(NODE _e "console.log(require('./src/app')('Test'))")
-
- if [ "$OUTPUT" == "$EXPECTED" ] ; then
-
- echo "Test passed!"
-
- exit 0 
- 
- else
-
- echo " Test failed! Expected "$EXPECTED' but got "$OUTPUT'"
-
-exit 1
-
+if [ "$OUTPUT" == "$EXPECTED" ]; then
+    echo "Test passed!"
+    exit 0
+else
+    echo "Test failed! Expected \"$EXPECTED\" but got \"$OUTPUT\""
+    exit 1
 fi
